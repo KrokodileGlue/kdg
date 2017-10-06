38ed568ee8bd9209b4c19310af3e5976e9da5ba8
KrokodileGlue <KrokodileGlue@outlook.com>
2017-10-05 17:40:54 -0700

    add posix character classes

--- ktre/ktre.h
+++ ktre/ktre.h
@@ -572,21 +572,13 @@ static struct node *term(struct ktre *re);
 static inline char
 lc(char c)
 {
-	if (c >= 'A' && c <= 'Z') {
-		c = (c - 'A') + 'a';
-	}
-
-	return c;
+	return c >= 'A' && c <= 'Z' ? (c - 'A') + 'a' : c;
 }
 
 static inline char
 uc(char c)
 {
-	if (c >= 'a' && c <= 'z') {
-		c = (c - 'a') + 'A';
-	}
-
-	return c;
+	return c >= 'a' && c <= 'z' ? (c - 'a') + 'A' : c;
 }
 
 static inline void
@@ -1108,12 +1100,56 @@ parse_group(struct ktre *re)
 	return left;
 }
 
+static struct {
+	char *name;
+	char *class;
+} pclasses[] = {
+	{ "[:upper:]", "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+	{ "[:lower:]", "abcdefghijklmnopqrstuvwxyz" },
+	{ "[:alpha:]", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+	{ "[:digit:]", "0123456789" },
+	{ "[:xdigit:]", "0123456789ABCDEFabcdef" },
+	{ "[:alnum:]", "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+	{ "[:punct:]", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" },
+	{ "[:blank:]", " \t" },
+	{ "[:space:]", WHITESPACE },
+	{ "[:cntrl:]", "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b"
+	  "\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19"
+	  "\x1a\x1b\x1c\x1d\x1e\x1f\x7F"},
+	{ "[:graph:]", "\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b"
+	  "\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a"
+	  "\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49"
+	  "\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58"
+	  "\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67"
+	  "\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76"
+	  "\x77\x78\x79\x7a\x7b\x7c\x7d\x7e"},
+	{ "[:print:]", "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b"
+	  "\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a"
+	  "\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49"
+	  "\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58"
+	  "\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67"
+	  "\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76"
+	  "\x77\x78\x79\x7a\x7b\x7c\x7d\x7e"}
+};
+
 static char *
 parse_character_class_character(struct ktre *re)
 {
 	char *a = NULL;
 
-	if (*re->sp != '\\') {
+	if (*re->sp == '[') {
+		for (int i = 0; i < sizeof pclasses / sizeof pclasses[0]; i++) {
+			if (!strncmp(re->sp, pclasses[i].name, strlen(pclasses[i].name))) {
+				append_str(re, &a, pclasses[i].class);
+				re->sp += strlen(pclasses[i].name);
+				return a;
+			}
+		}
+
+		append_char(re, &a, *re->sp);
+		next_char(re);
+		return a;
+	} else if (*re->sp != '\\') {
 		append_char(re, &a, *re->sp);
 		next_char(re);
 		return a;
@@ -1305,6 +1341,7 @@ parse_character_class(struct ktre *re)
 	if (*re->sp != ']') {
 		error(re, KTRE_ERROR_SYNTAX_ERROR, left->loc,
 		      "unterminated character class");
+		_free(class);
 		free_node(re, left);
 		return NULL;
 	}
