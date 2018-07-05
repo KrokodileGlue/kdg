b6878b4293f7e87d748c39ccd650ad649b45d29c
KrokodileGlue <KrokodileGlue@outlook.com>
2018-07-04 18:54:12 -0700

    Update unicode_data.c

--- script/gen.sh
+++ script/gen.sh
@@ -3,12 +3,14 @@
 
 VERSION=10.0.0
 
-wget https://www.unicode.org/Public/$VERSION/ucd/UnicodeData.txt
-wget https://www.unicode.org/Public/$VERSION/ucd/SpecialCasing.txt
-wget https://www.unicode.org/Public/$VERSION/ucd/DerivedCoreProperties.txt
-wget https://www.unicode.org/Public/$VERSION/ucd/CompositionExclusions.txt
-wget https://www.unicode.org/Public/$VERSION/ucd/CaseFolding.txt
-wget https://www.unicode.org/Public/$VERSION/ucd/auxiliary/GraphemeBreakProperty.txt
+if [ ! -f UnicodeData.txt ]; then
+    wget https://www.unicode.org/Public/$VERSION/ucd/UnicodeData.txt
+    wget https://www.unicode.org/Public/$VERSION/ucd/SpecialCasing.txt
+    wget https://www.unicode.org/Public/$VERSION/ucd/DerivedCoreProperties.txt
+    wget https://www.unicode.org/Public/$VERSION/ucd/CompositionExclusions.txt
+    wget https://www.unicode.org/Public/$VERSION/ucd/CaseFolding.txt
+    wget https://www.unicode.org/Public/$VERSION/ucd/auxiliary/GraphemeBreakProperty.txt
+fi
 
 ./gen.pl -v
 cp unicode_data.c ../src/
